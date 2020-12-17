52a31a820005bca2edb715ffc6a4df0392d87af3
PiangGG <piangpiangpiang@outlook.com>
2020-12-17 13:22:37 +0800

    init the game2

---
+++ Source/FleeFactory/FleeFactory.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FleeFactory.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FleeFactory, "FleeFactory" );
---
+++ Source/FleeFactory/FleeFactoryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FleeFactoryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLEEFACTORY_API AFleeFactoryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
